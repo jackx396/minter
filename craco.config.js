dce842b20264018a6c1ad790a8dabdd05b5ecf27
paperhand <paperhand9@gmail.com>
2025-09-05 18:27:11 +0530

    Add comprehensive webpack polyfills for all Node modules

--- craco.config.js
+++ craco.config.js
@@ -8,6 +8,14 @@ module.exports = {
         stream: require.resolve('stream-browserify'),
         buffer: require.resolve('buffer'),
         process: require.resolve('process/browser'),
+        util: require.resolve('util'),
+        url: require.resolve('url'),
+        assert: require.resolve('assert'),
+        http: false,
+        https: false,
+        os: false,
+        fs: false,
+        path: false,
       };
       
       webpackConfig.plugins.push(
